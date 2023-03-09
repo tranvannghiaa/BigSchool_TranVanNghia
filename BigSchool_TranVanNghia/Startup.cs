fbc4a4b25e631aa52d64eeb5f43c88f7c37a86d7
User <User@LAPTOP-O9BQMGQM>
2023-03-09 09:07:07 +0700

    Add project files.

---
+++ BigSchool_TranVanNghia/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BigSchool_TranVanNghia.Startup))]
+namespace BigSchool_TranVanNghia
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
