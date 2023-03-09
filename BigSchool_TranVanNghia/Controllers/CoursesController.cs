68f604a2926b9d13484fb2e0cd77046d3ed0617b
Tran Van Nghia <nghia147bao@gmail.com>
2023-03-09 10:59:19 +0700

    xong trang 35

---
+++ BigSchool_TranVanNghia/Controllers/CoursesController.cs
@@ -0,0 +1,29 @@
+﻿using BigSchool_TranVanNghia.Models;
+using BigSchool_TranVanNghia.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BigSchool_TranVanNghia.Controllers
+{
+    public class CoursesController : Controller
+    {
+        // GET: Courses
+        private readonly ApplicationDbContext _context;
+        public CoursesController() { 
+            _context=new ApplicationDbContext();
+        }
+        public ActionResult Create()
+        {
+            var viewModel = new CourseViewModel
+            {
+                Categories = _context.Categories.ToList()
+
+            };
+            
+            return View(viewModel);
+        }
+    }
+}
---
+++ BigSchool_TranVanNghia/Migrations/202303090303146_CreateTableCourse.cs
@@ -0,0 +1,43 @@
+﻿namespace BigSchool_TranVanNghia.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class CreateTableCourse : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Categories",
+                c => new
+                    {
+                        Id = c.Byte(nullable: false),
+                        Name = c.String(nullable: false, maxLength: 255),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Courses",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        LecturerId = c.String(nullable: false),
+                        Place = c.String(nullable: false, maxLength: 255),
+                        DateTime = c.DateTime(nullable: false),
+                        CategoryId = c.Byte(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Categories", t => t.CategoryId, cascadeDelete: true)
+                .Index(t => t.CategoryId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Courses", "CategoryId", "dbo.Categories");
+            DropIndex("dbo.Courses", new[] { "CategoryId" });
+            DropTable("dbo.Courses");
+            DropTable("dbo.Categories");
+        }
+    }
+}
